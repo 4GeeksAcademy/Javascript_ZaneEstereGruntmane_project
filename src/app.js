65b0767932ecd40ac38e70ee0c2b183e47603749
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-10-25 12:01:17 +0000

    excuse generator code improved according to the best practice guideline

--- src/app.js
+++ src/app.js
@@ -10,33 +10,29 @@ window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#the-excuse").innerHTML = generateExcuse();
   });
-  console.log("Hello Rigo from the console!");
-};
 
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
+  function generateExcuse() {
+    const whoSentence = getRandomSentence(whoCollection);
+    const actionSentence = getRandomSentence(actionCollection);
+    const whatSentence = getRandomSentence(whatCollection);
+    const whenSentence = getRandomSentence(whenCollection);
+
+    return `${getRandomSentence()} ${actionSentence} ${whatSentence} ${whenSentence}`;
+  }
+
+  function getRandomSentence(wholeCollection) {
+    const randomIndex = Math.floor(Math.random() * wholeCollection.length);
+    return wholeCollection[randomIndex];
+  }
+
+  const whoCollection = ["The dog", "My grandma", "His turtle", "My bird"];
+  const actionCollection = ["ate", "peed", "crushed", "broke"];
+  const whatCollection = ["my homework", "the keys", "the car"];
+  const whenCollection = [
     "before the class",
     "right on time",
     "when I finished",
     "during my lunch",
     "while I was praying"
   ];
-
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
-
-  return (
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex]
-  );
 };
