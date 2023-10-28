a1bc5b96071b7d6cdd1ddb0bc6d0a497969dc3fa
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-10-28 12:24:44 +0000

    stunning random excuse edited accordingly to the good practice guideline

--- src/app.js
+++ src/app.js
@@ -5,34 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const whoCollection = ["The dog", "My grandma", "His turtle", "My bird"];
+const actionCollection = ["ate", "peed", "crushed", "broke"];
+const whatCollection = ["my homework", "the keys", "the car"];
+const whenCollection = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+const getRandomValueFromArray = arr =>
+  arr[Math.floor(Math.random() * arr.length)];
+const generateExcuse = (who, action, what, when) => {
+  const excuse = `${getRandomValueFromArray(who)} ${getRandomValueFromArray(
+    action
+  )} ${getRandomValueFromArray(what)} ${getRandomValueFromArray(when)}`;
+  return excuse;
+};
+
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse(
+    whoCollection,
+    actionCollection,
+    whatCollection,
+    whenCollection
+  );
+});
+
 window.onload = () => {
-  //write your code here
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#the-excuse").innerHTML = generateExcuse();
-  });
-
-  function generateExcuse() {
-    const whoSentence = getRandomSentence(whoCollection);
-    const actionSentence = getRandomSentence(actionCollection);
-    const whatSentence = getRandomSentence(whatCollection);
-    const whenSentence = getRandomSentence(whenCollection);
-
-    return `${getRandomSentence()} ${actionSentence} ${whatSentence} ${whenSentence}`;
-  }
-
-  function getRandomSentence(wholeCollection) {
-    const randomIndex = Math.floor(Math.random() * wholeCollection.length);
-    return wholeCollection[randomIndex];
-  }
-
-  const whoCollection = ["The dog", "My grandma", "His turtle", "My bird"];
-  const actionCollection = ["ate", "peed", "crushed", "broke"];
-  const whatCollection = ["my homework", "the keys", "the car"];
-  const whenCollection = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  document.querySelector("#the-excuse").innerHTML = generateExcuse(
+    whoCollection,
+    actionCollection,
+    whatCollection,
+    whenCollection
+  );
 };
